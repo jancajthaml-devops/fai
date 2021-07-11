ffcf7aa8e9c872038752a120a50ffdbb050e6b01
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-07-11 17:06:11 +0200

    wip

--- class/20-hwdetect.sh
+++ class/20-hwdetect.sh
@@ -2,29 +2,35 @@
 
 # (c) Thomas Lange, 2002-2013, lange@informatik.uni-koeln.de
 
-# NOTE: Files named *.source will be evaluated, but their output ignored. Instead
-# the contents of $newclasses will be added to the list of defined classes.
+# NOTE: Files named *.sh will be evaluated, but their output ignored.
 
 [ $do_init_tasks -eq 1 ] || return 0 # Do only execute when doing install
 
 echo 0 > /proc/sys/kernel/printk
 
-kernelmodules="sd_mod sr_mod megaraid_sas"
+#kernelmodules=
 # here, you can load modules depending on the kernel version
 case $(uname -r) in
     2.6*) kernelmodules="$kernelmodules mptspi dm-mod md-mod aes dm-crypt" ;;
-      3*) kernelmodules="$kernelmodules mptspi dm-mod md-mod aes dm-crypt" ;;
+    [3456]*) kernelmodules="$kernelmodules mptspi dm-mod md-mod aes dm-crypt" ;;
 esac
 
-for mod in $kernelmodules ; do
-    [ "$verbose" ] && echo Loading kernel module $mod
+for mod in $kernelmodules; do
+    [ X$verbose = X1 ] && echo Loading kernel module $mod
     modprobe -a $mod 1>/dev/null 2>&1
 done
 
-ip ad show up | egrep -iv 'loopback|127.0.0.1|::1/128|_lft'
+# show the basic information about the network interface
+ip -br li show up|egrep -v ^lo; ip -br a show up|egrep -v ^lo
 
 echo $printk > /proc/sys/kernel/printk
 
-set_disk_info  # calculate number of available disks
+odisklist=$disklist
+set_disk_info  # recalculate list of available disks
+if [ "$disklist" != "$odisklist" ]; then
+    echo New disklist: $disklist
+    echo disklist=\"$disklist\" >> $LOGDIR/additional.var
+fi
+
 save_dmesg     # save new boot messages (from loading modules)
 
