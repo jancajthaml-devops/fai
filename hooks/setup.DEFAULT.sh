1e4e87b4130bd9f65e7608072bf88a4e3912b9b7
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-07-12 07:50:52 +0200

    wip

--- hooks/setup.DEFAULT.sh
+++ hooks/setup.DEFAULT.sh
@@ -2,6 +2,6 @@
 
 # use short hostname instead of FQDN
 export HOSTNAME=${HOSTNAME%%.*}
-if [ $do_init_tasks -eq 1 ]; then
+if [ $do_init_tasks -eq 1 ] ; then
   echo $HOSTNAME > /proc/sys/kernel/hostname
 fi
